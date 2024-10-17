abab5268b7c1d496b2b36417908460fbcf92da38
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-10-16 21:20:18 -0400

    add async function to post comments, edit event listener accordingly, and edit timestamp innerText to create dynamic timestamp

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,31 +2,7 @@ const API_KEY = "41d6b603-0b2e-403a-809e-fa896643e118";
 
 const bandSiteApi = new BandSiteApi(API_KEY);
 
-async function testAPICall() {
-    try {
-        const comments = await bandSiteApi.getComments();
-        console.log(comments);
-    } catch (error) {
-        console.error(error);
-    }
-    try {
-        await bandSiteApi.postComment({
-            name: "Jane Doe",
-            comment:
-                "This is some text that I have created for the purposes of testing this API call into a postComment async function. Hopefully it works!",
-        });
-
-        const updatedComments = await bandSiteApi.getComments();
-
-        console.log(updatedComments);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-// testAPICall();
-
-// =================================================================================
+// ============================================================================
 
 // pointing to <ul> element (aka comment section container)
 let listEl = document.querySelector(".comment__list");
@@ -52,7 +28,7 @@ function createCommentPost(commentObj) {
 
     let dateParaEl = document.createElement("p");
     dateParaEl.classList.add("comment__date");
-    dateParaEl.innerText = commentObj.timestamp;
+    dateParaEl.innerText = convertTime(commentObj.timestamp);
 
     let commDivEl = document.createElement("div");
     commDivEl.classList.add("comment__content-container");
@@ -76,97 +52,7 @@ function createCommentPost(commentObj) {
     commDivEl.appendChild(commParaEl);
 }
 
-// create function that will render the comment posts for each array object
-async function renderComment() {
-    try {
-        let commentData = await bandSiteApi.getComments();
-
-        commentData.forEach((commentItem) => {
-            convertTime(commentItem.timestamp);
-            createCommentPost(commentItem);
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-// invoke the function to ensure existing comments are initially rendered
-renderComment();
-
-// pointing to <form> element
-let form = document.querySelector(".form");
-
-// add event submit event listener for the <form> element
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let valueUserName = e.target.userName.value;
-    let valueUserComment = e.target.userComment.value;
-
-    let submissionDate = new Date();
-    let formattedDate = submissionDate.toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-    });
-
-    // validate form submission by checking for empty strings
-    let allErrors = document.querySelectorAll(".error");
-    let nameError = allErrors[0];
-    let commentError = allErrors[1];
-
-    nameError.innerHTML = "";
-    commentError.innerHTML = "";
-
-    nameError.classList.remove("error--active");
-    commentError.classList.remove("error--active");
-
-    errors = [];
-
-    // notify users of invalid inputs
-    if (valueUserName === "") {
-        errors.push("Please enter a valid name");
-    }
-
-    if (valueUserComment === "") {
-        errors.push("Please enter a valid comment");
-    }
-
-    if (errors.length === 2) {
-        if (errors[0]) {
-            nameError.innerHTML = errors[0];
-            nameError.classList.add("error--active");
-        }
-        if (errors[1]) {
-            commentError.innerHTML = errors[1];
-            commentError.classList.add("error--active");
-        }
-        return;
-    } else if (errors.length === 1) {
-        if (valueUserName === "") {
-            nameError.innerHTML = errors[0];
-            nameError.classList.add("error--active");
-        } else {
-            commentError.innerHTML = errors[0];
-            commentError.classList.add("error--active");
-        }
-        return;
-    } else {
-        // add the new comment post to the beginning of the array of object (i.e. index 0)
-        commentList.unshift({
-            name: valueUserName,
-            comment: valueUserComment,
-            timestamp: formattedDate,
-        });
-
-        // invoke the render function again to display the newly submitted comment
-        renderComment();
-
-        // clear the input fields to signal a complete submission
-        form.reset();
-    }
-});
-
+// create function to convert EPOCH timestamp into a dynamic, more human-readable timestamp
 function convertTime(timestamp) {
     let timeDiff = Date.now() - timestamp;
     let calculatedDiff;
@@ -211,4 +97,101 @@ function convertTime(timestamp) {
     return formattedDate;
 }
 
-console.log(convertTime(1729124091000));
+// ============================================================================
+
+// create async function that will render the comment posts for each array object
+async function renderComment() {
+    try {
+        let commentData = await bandSiteApi.getComments();
+
+        commentData.forEach((commentItem) => {
+            createCommentPost(commentItem);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// invoke the function to ensure existing comments are initially rendered
+renderComment();
+
+// create async function that will post a comment, adding it to the comment array of objects
+async function addComment(newCommentObj) {
+    try {
+        await bandSiteApi.postComment(newCommentObj);
+
+        listEl.replaceChildren();
+
+        renderComment();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// ============================================================================
+
+// pointing to <form> element
+let form = document.querySelector(".form");
+
+// add event submit event listener for the <form> element
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let valueUserName = e.target.userName.value;
+    let valueUserComment = e.target.userComment.value;
+
+    // store the form values into an object
+    let submissionComment = {
+        name: valueUserName,
+        comment: valueUserComment,
+    };
+
+    // validate form submission by checking for empty strings
+    let allErrors = document.querySelectorAll(".error");
+    let nameError = allErrors[0];
+    let commentError = allErrors[1];
+
+    nameError.innerHTML = "";
+    commentError.innerHTML = "";
+
+    nameError.classList.remove("error--active");
+    commentError.classList.remove("error--active");
+
+    errors = [];
+
+    // notify users of invalid inputs
+    if (!valueUserName) {
+        errors.push("Please enter a valid name");
+    }
+
+    if (!valueUserComment) {
+        errors.push("Please enter a valid comment");
+    }
+
+    if (errors.length === 2) {
+        if (errors[0]) {
+            nameError.innerHTML = errors[0];
+            nameError.classList.add("error--active");
+        }
+        if (errors[1]) {
+            commentError.innerHTML = errors[1];
+            commentError.classList.add("error--active");
+        }
+        return;
+    } else if (errors.length === 1) {
+        if (valueUserName === "") {
+            nameError.innerHTML = errors[0];
+            nameError.classList.add("error--active");
+        } else {
+            commentError.innerHTML = errors[0];
+            commentError.classList.add("error--active");
+        }
+        return;
+    } else {
+        // invoke function to post form contents once submission is valid
+        addComment(submissionComment);
+
+        // clear the input fields to signal a complete submission
+        form.reset();
+    }
+});
